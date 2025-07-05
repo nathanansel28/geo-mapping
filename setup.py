a68b2c9f5456f4cc9aaf61b47bd9e4d7aba0051a
Nathan Ansel <nathanansel.edu@gmail.com>
2025-07-06 02:13:55 +0800

    feat: project setup

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-import os
+from typing import List
 from download_shapefiles import download_natural_earth_populated_places
+import os
 
-def setup():
+def setup_and_fetch_variables() -> dict:
     download_natural_earth_populated_places()
-    os.makedirs("output")
+    os.makedirs("output", exist_ok=True)
+    folder_paths = os.environ.get("FOLDER_PATH")
+    folder_paths = folder_paths.split(",") if folder_paths else []
+
+    env_variables = {
+        "folder_paths": folder_paths
+    }
+    return env_variables
     
