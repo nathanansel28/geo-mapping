9cd4d721553716aaf0000aa07098372f45150821
Nathan Ansel <nathanansel.edu@gmail.com>
2025-07-21 01:43:55 +0800

    feat: download assets only if it has not been downloaded

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from download_shapefiles import download_natural_earth_populated_places
 import os
 
 def setup_and_fetch_variables() -> dict:
-    download_natural_earth_populated_places()
+    if not os.path.exists("assets/ne_10m_populated_places.shp"):
+        download_natural_earth_populated_places()
     os.makedirs("output", exist_ok=True)
     folder_paths = os.environ.get("FOLDER_PATH")
     folder_paths = folder_paths.split(",") if folder_paths else []
